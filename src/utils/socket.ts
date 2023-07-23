206e393976c17802a494b8d0646b1673fd73cf4d
ShivangM <shivangmishra0824@gmail.com>
2023-07-24 00:33:14 +0530

    Backend URL Updated

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_BACKEND_URL!
+    ? process.env.REACT_APP_BACKEND_URL!
     : 'http://localhost:5000';
 
 export const socket = io(URL);
