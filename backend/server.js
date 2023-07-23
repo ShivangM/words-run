d8e41b3ce4936b0103e734364b440facfcf102e5
ShivangM <shivangmishra0824@gmail.com>
2023-07-24 00:36:44 +0530

    Cors Origin Fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://words-run.vercel.app'],
     methods: ['GET', 'POST'],
   },
 });
